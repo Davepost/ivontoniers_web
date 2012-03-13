08739bb358bb6f5e580938c75246952255c34192
Davepost <davepost1990@hotmail.com>
2012-03-13 11:42:01 +0000

    User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,19 @@ class UsersController < ApplicationController
   def new
       @user = User.new
     end
-       def create
-         @user = User.new(params[:user])
-         if @user.save
-           sign_in @user
-           flash[:success] = "Welcome to the Sample App!"
-           redirect_to @user
-         else
-           render 'new'
-         end
-       end
-     end
+
+ 
+     def create
+        @user = User.new(params[:user])
+        if @user.save
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
+        else
+          render 'new'
+        end
+      end
+    end
+ 
+  
      
 
